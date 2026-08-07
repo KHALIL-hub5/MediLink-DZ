3679f260e3290b3a92a98c97ea2ba9147115c8f0
KHALIL-hub5 <khalildjaidja510@gmail.com>
2026-08-07 12:14:40 +0100

    add development seed data

--- Back-end/prisma.config.ts
+++ Back-end/prisma.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma',
-
   migrations: {
     path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
   },
 });
